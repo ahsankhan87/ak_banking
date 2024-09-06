c2738080a8905b2d6b2ed6349c93e28989c41d3b
Ahsan Khan <ahsankhan50@gmail.com>
2024-09-06 11:39:32 +0500

    add sentry auth token in env

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,9 @@ export default withSentryConfig(nextConfig, {
 org: "khybersoft",
 project: "javascript-nextjs",
 
+// An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
 
